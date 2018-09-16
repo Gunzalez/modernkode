c6a5d4f4aedd29ee8d1ba6e8d2a6cf51e49b06ea
Segun Konibire <gunzalez@hotmail.com>
2018-09-16 07:58:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Project Berry Good
+
+[Link - gunzalez.com/pp/berrygood](http://www.gunzalez.com/pp/berrygood)
+
---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+// grab our gulp packages
+var gulp  = require('gulp');
+var gutil = require('gulp-util');
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+// configure the jshint task
+gulp.task('jshint', function() {
+    return gulp.src('website/js/*.js')
+            .pipe(jshint())
+            .pipe(jshint.reporter('jshint-stylish'))
+            .pipe(browserSync.reload({
+                stream: true
+            }));
+});
+
+// configure sass task
+gulp.task('sass', function(){
+    return gulp.src('source/scss/**/*.scss')
+            .pipe(sass())
+            .pipe(gulp.dest('website/css'))
+            .pipe(browserSync.reload({
+                stream: true
+            }));
+});
+
+// start web server
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'website'
+        }
+    })
+});
+
+// watch changes to HTML, CSS and JS,
+gulp.task('watch', ['browserSync'], function (){
+    gulp.watch('source/scss/**/*.scss', ['sass']);
+    gulp.watch('website/js/*.js', ['jshint']);
+    gulp.watch('website/*.html', browserSync.reload);
+});
+
+// define the default task and add the watch task to it
+gulp.task('default', ['watch']);
+
+
+
+
+
