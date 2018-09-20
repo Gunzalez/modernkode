34950b80c2f8d6f73629f7062f22050eebe81247
Segun Konibire <gunzalez@hotmail.com>
2018-09-20 13:10:12 +0100

    code refactor

--- website/js/modernkode.js
+++ website/js/modernkode.js
@@ -1,65 +1,36 @@
 // JavaScript Document
 (function (window) {
 
-    let modernkode = {};
-
-    modernkode.properties = {
-        windowWidth: '',
-        isMobile: false
-    };
+    var modernkode = {};
+    window['modernkode'] = modernkode;
 
     modernkode.utils = {
         //
     };
 
     modernkode.environment = {
-
-        resize: function(){
-            //
-        },
-
         init: function (){
             modernkode['ribbon'] = document.getElementById('ribbon');
             modernkode['sticky'] = modernkode['ribbon'].offsetTop;
         }
     };
 
-    modernkode.init = function () {
-
-        // all init here
+    // main init
+    modernkode.init = function () { // all init here
         modernkode.environment.init();
-
-        // // resize triggers
-        // $(window).on('resize', function () {
-        //
-        //     var newWidth = $(window).width(),
-        //         oldWidth = modernkode.properties.windowWidth;
-        //
-        //     if (oldWidth !== newWidth) {
-        //         modernkode.properties.windowWidth = newWidth;
-        //         modernkode.resize();
-        //     }
-        // });
-        //
-        // // trigger initial resize, just to be sure
-        // modernkode.resize();
-        // $(window).trigger('resize');
     };
 
     // main resize
-    modernkode.resize = function () {
+    modernkode.resize = function () { // all resize here
         modernkode.environment.resize();
     };
 
-    modernkode.scrollEvents = function(){
-
-        var ribbon = modernkode['ribbon'],
-            header = ribbon.parentNode;
-
+    // main scroll
+    modernkode.scrollEvents = function(){ // all scroll here
         if (window.pageYOffset > modernkode['sticky']) {
-            header.classList.add("off-screen");
+            modernkode['ribbon'].parentNode.classList.add("off-screen");
         } else {
-            header.classList.remove("off-screen");
+            modernkode['ribbon'].parentNode.classList.remove("off-screen");
         }
     };
 
@@ -69,8 +40,6 @@
         window['onscroll'] = function() {
             modernkode.scrollEvents();
         };
-
     });
-
 
 }(window));
