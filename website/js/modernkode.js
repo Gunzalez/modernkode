ce4fe859df64d3365168fe2726e333c92e72abff
Segun Konibire <gunzalez@hotmail.com>
2018-09-22 02:33:11 +0100

    code tidy

--- website/js/modernkode.js
+++ website/js/modernkode.js
@@ -4,9 +4,6 @@
     var modernkode = {};
     window.modernkode = modernkode;
 
-    modernkode.utils = {
-    };
-
     modernkode.environment = {
         init: function (){
 
@@ -17,9 +14,12 @@
             // desktop colour/bw fun
             modernkode.desktopTrigger = document.querySelector('#control');
             modernkode.desktopTrigger.onclick = function () {
-                modernkode.desktopTrigger.parentNode.classList.toggle('off');
+                modernkode.desktopTrigger.parentNode['classList'].toggle('off');
                 document.querySelector('#controlled').classList.toggle('off');
             }
+
+            // apps border fun
+
         }
     };
 
@@ -28,17 +28,12 @@
         modernkode.environment.init();
     };
 
-    // main resize
-    modernkode.resize = function () { // all resize here
-        modernkode.environment.resize();
-    };
-
     // main scroll
     modernkode.scrollEvents = function(){ // all scroll here
         if (window.pageYOffset > modernkode.sticky) {
-            modernkode.ribbon.parentNode.classList.add("off-screen");
+            modernkode.ribbon.parentNode['classList'].add("off-screen");
         } else {
-            modernkode.ribbon.parentNode.classList.remove("off-screen");
+            modernkode.ribbon.parentNode['classList'].remove("off-screen");
         }
     };
 
