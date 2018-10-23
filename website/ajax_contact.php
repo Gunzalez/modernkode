6c39a1527796b18608237ce3c9eee9ae717a8674
Segun Konibire <gunzalez@hotmail.com>
2018-10-23 19:24:59 +0100

    adds index.php

---
+++ website/ajax_contact.php
@@ -0,0 +1,80 @@
+<?php
+    /**
+     * on the form page add php code
+     * 
+     * session_start();
+     * $kode = md5(mt_rand());
+     * 
+     * then add a hidden field in the form for the kode var
+     * <input type="hidden" name="kode" value="<?php echo $kode?>" />
+     */
+    
+    $to      = 'gunzalez@gmail.com';
+    $subject = 'Modernkode: Website enquiry';
+    $headers = [
+        'From' => 'gunzalez@hotmail.com',
+        'Reply-To' => 'gunzalez@hotmail.com',
+        'X-Mailer' => 'ModernKode'
+    ];
+    $errors = [];
+    $message = '';
+
+    session_start();
+
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+        exit;
+    }
+
+    // test for ajax post //
+    if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) || $_SERVER['HTTP_X_REQUESTED_WITH'] !== 'XMLHttpRequest') {
+        exit;
+    }
+
+    // xss test //
+    if (!isset($_SESSION['kode'])) {
+        exit;
+    }
+    if ($_SESSION['kode'] !== $_POST['kode']) {
+        exit;
+    }
+
+    
+    // validate posted data //
+    if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = 'Email is not valid';
+    }
+    else if (empty($_POST['name'])) {
+        $errors['name'] = 'Name is a requied field';
+    }
+    else if (empty($_POST['message'])) {
+        $errors['message'] = 'Message is a requied field';
+    }
+    
+    // send mail //
+    if (empty($errors)) {
+        foreach ($_POST as $name => $value) {
+            $message .= "{$name}: {$value}\n";
+        }
+
+        if (mail($to, $subject, $message, $headers)) {
+            $response = [
+                'status' => 'success',
+                'msg' => "Thanks your message has been sent, we'll be in touch very soon."
+            ];
+        }
+        else {
+            $errors['system'] = 'Sorry your email could not be sent at the moment, please try later.';
+        }
+    }
+    else {
+        $response = [
+            'status' => 'error',
+            'msg' => "Sorry there was a problem.",
+            'errors' => $errors
+        ];
+    }
+
+    // send response //
+    header('Content-type: application/json');
+    echo json_encode($response);
+    
---
+++ website/index.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>MODERN KODE</title>
+    <meta name="Description" content="We provide mobile and web solutions for SMEs.">
+    <meta name="Keywords" content="Mobile, Apps, PWAs, websites, iOS, Android, React, React Native, Angular, Ionic, Vue, Segun Konibire, Modern">
+
+    <link href="css/reset.css" rel="stylesheet">
+    <link href="css/modernkode.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+
+</head>
+<body>
+    <div class="header">
+        <div class="hero">
+
+            <div class="text">
+                <img src="images/mk-logo-white.svg" class="logo" alt="Modern Kode logo" />
+                <p>
+                    Hello, we are MODERN KODE.<br>
+                        We provide mobile and web solutions.<br>
+                    We are big on small screens.
+                   </p>
+            </div>
+
+        </div>
+        <div class="ribbon" id="ribbon">
+
+            <div class="menu">
+                <div class="item">
+                    <h1><img src="images/mk-logo.svg" class="logo" alt="Modern Kode logo" /> MODERN KODE</h1>
+                </div>
+                <div class="item">
+                    <ul>
+                        <li>
+                            <a href="tel:07852263922" class="number-string">
+                                <i class="fas fa-phone"></i>
+                                <span>+44&nbsp;785&nbsp;226&nbsp;3922</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="mailto:&#105;&#110;&#102;&#111;&#064;&#109;&#111;&#100;&#101;&#114;&#110;&#107;&#111;&#100;&#101;&#046;&#099;&#111;&#109;">
+                                <i class="far fa-envelope"></i>
+                                <span>&#105;&#110;&#102;&#111;&#064;&#109;&#111;&#100;&#101;&#114;&#110;&#107;&#111;&#100;&#101;&#046;&#099;&#111;&#109;</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="section _mobile">
+
+        <div class="text">
+            <h2>You &amp; your problems</h2>
+            <p>
+                <span>Where our clients may see problems, we see opportunities.</span>
+                <span>We don’t just work for you, we work <em>with</em> you.</span>
+            </p>
+        </div>
+
+        <div class="showcase">
+            <div class="item">
+                <h4>OnBoarding_</h4>
+                <p>Company staff introduction</p>
+                <img src="images/onboarding-4.jpg" id="mobile-1" alt="OnBoarding App" class="apps" data-bg="1" />
+                <div class="switches loading">
+                    <a href="images/onboarding-4.jpg" data-target="mobile-1" class="app active">1</a>
+                    <a href="images/onboarding-2.jpg" data-target="mobile-1" class="app">2</a>
+                    <a href="images/onboarding-3.jpg" data-target="mobile-1" class="app">3</a>
+                    <a href="images/onboarding-5.jpg" data-target="mobile-1" class="app">4</a>
+                    <a href="images/onboarding-0.jpg" data-target="mobile-1" class="app">5</a>
+                </div>
+            </div>
+            <div class="item">
+                <h4>Saving Pots</h4>
+                <p>Group savings schemes</p>
+                <img src="images/saving-pots-2.jpg" id="mobile-2" alt="Saving Pots" class="apps" data-bg="2" />
+                <div class="switches loading">
+                    <a href="images/saving-pots-2.jpg" data-target="mobile-2" class="app active">1</a>
+                    <a href="images/saving-pots-1.jpg" data-target="mobile-2" class="app">2</a>
+                    <a href="images/saving-pots-4.jpg" data-target="mobile-2" class="app">3</a>
+                    <a href="images/saving-pots-3.jpg" data-target="mobile-2" class="app">4</a>
+                    <a href="images/saving-pots-0.jpg" data-target="mobile-2" class="app">5</a>
+                </div>
+            </div>
+            <div class="item">
+                <h4>Pounded Yam</h4>
+                <p>Meals, Recipes & Restaurants</p>
+                <img src="images/pounded-yam-1.jpg" id="mobile-3" alt="Pounded Yam" class="apps" data-bg="3" />
+                <div class="switches loading">
+                    <a href="images/pounded-yam-1.jpg" data-target="mobile-3" class="app active">1</a>
+                    <a href="images/pounded-yam-2.jpg" data-target="mobile-3" class="app">2</a>
+                    <a href="images/pounded-yam-3.jpg" data-target="mobile-3" class="app">3</a>
+                    <a href="images/pounded-yam-4.jpg" data-target="mobile-3" class="app">4</a>
+                    <a href="images/pounded-yam-0.jpg" data-target="mobile-3" class="app">5</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section _desktop">
+
+        <div class="text">
+            <h2>We create websites too</h2>
+            <p>We think mobile first and so build mobile websites that work well on desktops.</p>
+        </div>
+
+        <div id="controlled" class="showcase off">
+            <div class="item">
+                <img src="images/work-convenire.jpg" alt="Convenire" />
+                <!--<h4><a href="#">Convenire</a></h4>-->
+            </div>
+            <div class="item">
+                <img src="images/work-murray-machinery.jpg" alt="Murray Machinery" />
+                <!--<h4><a href="#">Murray Machinery</a></h4>-->
+            </div>
+            <div class="item">
+                <img src="images/work-bumi-thomas.jpg" alt="Bumi Music" />
+                <!--<h4><a href="#">Bumi Music</a></h4>-->
+            </div>
+            <div class="item">
+                <img src="images/work-patternstudios.jpg" alt="Pattern Studios" />
+                <!--<h4>Pattern Studios</h4>-->
+            </div>
+            <div class="item">
+                <img src="images/work-pounding.jpg" alt="Pound That Yam" />
+                <!--<h4>Pound That Yam</h4>-->
+            </div>
+        </div>
+
+        <div class="controls off" aria-hidden="true">
+            <button id="control" data-device="desktop-only">
+                <span>COLOR</span>
+                <span>MONOCHROME</span>
+            </button>
+        </div>
+
+    </div>
+
+    <div class="section _technology">
+        <div class="text">
+            <h2>Technology stack</h2>
+            <p>
+                <span>Wherever possible we use Open Source technology, means free to use.</span>
+                <span>This keeps costs down as clients only pay for labour.</span>
+            </p>
+        </div>
+
+        <div class="showcase">
+            <div class="item">
+                <img src="images/logo-angular.png" alt="Angular" />
+                <p>Angular</p>
+            </div>
+            <div class="item">
+                <img src="images/logo-ionic.png" alt="Ionic" />
+                <p>Ionic</p>
+            </div>
+            <div class="item">
+                <img src="images/logo-react.png" alt="React and React Native" />
+                <p>React Native</p>
+            </div>
+            <div class="item"><img src="images/logo-vue.png" alt="Vue" />
+                <p>Vue</p>
+            </div>
+            <div class="item"><img src="images/logo-swift.png" alt="Swift" />
+                <p>Swift</p>
+            </div>
+            <div class="item"><img src="images/logo-corona.png" alt="Corona" />
+                <p>Corona Labs</p>
+            </div>
+            <div class="item"><img src="images/logo-node-js.png" alt="Node" />
+                <p>Node</p>
+            </div>
+            <div class="item"><img src="images/logo-jquery.png" alt="jQuery" />
+                <p>jQuery</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="section _contact">
+        <div class="text">
+            <h2>Get in touch</h2>
+            <p>
+                Call us on <a href="tel:07852263922" class="number-string">+44&nbsp;785&nbsp;226&nbsp;3922</a><br>
+                Send emails to <a href="mailto:info@mordernkode.com">&#105;&#110;&#102;&#111;&#064;&#109;&#111;&#100;&#101;&#114;&#110;&#107;&#111;&#100;&#101;&#046;&#099;&#111;&#109;</a>
+            </p>
+        </div>
+
+        <form id="contact-form" action="ajax_contact.php" method="POST">
+
+            <?php
+                session_start();
+                $kode = md5(mt_rand());
+            ?>
+            <input type="hidden" name="kode" value="<?php echo $kode?>” />
+
+            <div class="fields">
+                <div class="row">
+                    <label for="name" class="visually-hidden">Name</label>
+                    <input type="text" id="name" value="" placeholder="Name" />
+                </div>
+                <div class="row">
+                    <label for="email" class="visually-hidden">Email</label>
+                    <input type="email" id="email" value="" placeholder="Email address" />
+                </div>
+                <div class="row">
+                    <label for="message" class="visually-hidden">Message</label>
+                    <textarea id="message" placeholder="Say anything in here"></textarea>
+                </div>
+                <div class="row _submit">
+                    <!--<input type="button" class="button" value="Send" />-->
+                    <button id="send">Send email</button>
+                </div>
+            </div>
+        </form>
+
+    </div>
+
+    <div class="footer">
+        <div class="content">
+            <div class="item">
+                <h4>Watch this space!</h4>
+                <ul class="bullets">
+                    <li>NOW: Saving Pots v2 - A redesign, and from Ionic to React Native</li>
+                    <li>NEXT: A tailoring app for traditional designer outfits</li>
+                    <li>COMING SOON: Mobile gaming app, based on a popular UK motorway</li>
+                </ul>
+            </div>
+            <div class="item">
+                <h4>Contact us</h4>
+                <ul class="contact-links">
+                    <li><a href="tel:07852263922"><i class="fas fa-phone-square"></i></a></li>
+                    <li><a href="mailto:info@mordernkode.com"><i class="fas fa-envelope-square"></i></a></li>
+                </ul>
+            </div>
+            <div class="item">
+                <h4>Social media</h4>
+                <ul class="social-links">
+                    <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+                    <li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+                    <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="company">
+            <p>Copyright &copy; <span class="number-string">2018</span> MODERN KODE Ltd.
+                <br />All rights reserved.</p>
+            <p>Company No. <span class="number-string">10594333</span></p>
+        </div>
+    </div>
+    <script src="js/domready.js"></script>
+    <script src="js/modernkode.js"></script>
+</body>
+</html>
